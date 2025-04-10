ce64f1482b9dbfb2d659bc4c9b085b21550b9afe
Daniil <larochkin.daniil@gmail.com>
2025-04-10 20:57:07 +0300

    Задача

---
+++ code.js
@@ -0,0 +1,24 @@
+let str = 'str'
+let num = 3
+let bool = true
+let nul = null
+let und = undefined
+let obj = {}
+let arr = []
+let sym = symbol
+let big = 10452235235325n
+
+
+let test = 0
+test = 2
+
+const TEST = 0
+TEST = 2
+
+
+let newArr = [str, num, bool, nul, und, obj, arr, sym, big]
+
+let newObj = {
+    str: str,
+    num: num
+}
